24722d7c0bd5d1d362b4c8e53b1ad40da823af66
Fernando Aparicio <faparicior@gmail.com>
2024-05-02 22:02:25 +0200

    Use block in tests

--- kotlin/src/test/kotlin/e2e/AdvertisementTest.kt
+++ kotlin/src/test/kotlin/e2e/AdvertisementTest.kt
@@ -36,14 +36,14 @@ class AdvertisementTest {
         val server = Server(DependencyInjectionResolver())
 
         val result = server.route(FrameworkRequest(
-                FrameworkRequest.METHOD_POST,
-                "advertisement",
-                mapOf(
-                    "id" to ID,
-                    "description" to DESCRIPTION,
-                    "password" to PASSWORD,
-                )
+            FrameworkRequest.METHOD_POST,
+            "advertisement",
+            mapOf(
+                "id" to ID,
+                "description" to DESCRIPTION,
+                "password" to PASSWORD,
             )
+        )
         )
 
         Assertions.assertEquals(FrameworkResponse.STATUS_CREATED, result.statusCode)
@@ -60,132 +60,135 @@ class AdvertisementTest {
 
     @Test
     fun `should update an advertisement`() {
+        withAnAdvertisementCreated {
+            val server = Server(DependencyInjectionResolver())
 
-        withAnAdvertisementCreated()
-
-        val server = Server(DependencyInjectionResolver())
-
-        val result = server.route(FrameworkRequest(
-                FrameworkRequest.METHOD_PUT,
-                "advertisement/$ID",
-                mapOf(
-                    "description" to NEW_DESCRIPTION,
-                    "password" to PASSWORD,
+            val result = server.route(
+                FrameworkRequest(
+                    FrameworkRequest.METHOD_PUT,
+                    "advertisement/$ID",
+                    mapOf(
+                        "description" to NEW_DESCRIPTION,
+                        "password" to PASSWORD,
+                    )
                 )
             )
-        )
 
-        Assertions.assertEquals(FrameworkResponse.STATUS_OK, result.statusCode)
+            Assertions.assertEquals(FrameworkResponse.STATUS_OK, result.statusCode)
 
-        val resultSet = this.connection.query("SELECT * from advertisements;")
-        var description = ""
-        var date: LocalDateTime? = null
+            val resultSet = this.connection.query("SELECT * from advertisements;")
+            var description = ""
+            var date: LocalDateTime? = null
 
-        if (resultSet.next()) {
-            description = resultSet.getString("description")
-            date = LocalDateTime.parse(resultSet.getString("advertisement_date"))
+            if (resultSet.next()) {
+                description = resultSet.getString("description")
+                date = LocalDateTime.parse(resultSet.getString("advertisement_date"))
+            }
+
+            Assertions.assertEquals(NEW_DESCRIPTION, description)
+            Assertions.assertNotNull(date)
+            Assertions.assertTrue(date!!.isAfter(LocalDateTime.parse(ADVERTISEMENT_CREATION_DATE)))
         }
-
-        Assertions.assertEquals(NEW_DESCRIPTION, description)
-        Assertions.assertNotNull(date)
-        Assertions.assertTrue(date!!.isAfter(LocalDateTime.parse(ADVERTISEMENT_CREATION_DATE)))
     }
 
     @Test
     fun `should renew an advertisement`() {
+        withAnAdvertisementCreated {
+            val server = Server(DependencyInjectionResolver())
 
-        withAnAdvertisementCreated()
-
-        val server = Server(DependencyInjectionResolver())
-
-        val result = server.route(FrameworkRequest(
-                FrameworkRequest.METHOD_PATCH,
-                "advertisement/$ID",
-                mapOf(
-                    "password" to PASSWORD,
+            val result = server.route(
+                FrameworkRequest(
+                    FrameworkRequest.METHOD_PATCH,
+                    "advertisement/$ID",
+                    mapOf(
+                        "password" to PASSWORD,
+                    )
                 )
             )
-        )
 
-        Assertions.assertEquals(FrameworkResponse.STATUS_OK, result.statusCode)
+            Assertions.assertEquals(FrameworkResponse.STATUS_OK, result.statusCode)
 
-        val resultSet = this.connection.query("SELECT * from advertisements;")
-        var date: LocalDateTime? = null
+            val resultSet = this.connection.query("SELECT * from advertisements;")
+            var date: LocalDateTime? = null
 
-        if (resultSet.next()) {
-            date = LocalDateTime.parse(resultSet.getString("advertisement_date"))
+            if (resultSet.next()) {
+                date = LocalDateTime.parse(resultSet.getString("advertisement_date"))
+            }
+
+            Assertions.assertNotNull(date)
+            Assertions.assertTrue(date!!.isAfter(LocalDateTime.parse(ADVERTISEMENT_CREATION_DATE)))
         }
-
-        Assertions.assertNotNull(date)
-        Assertions.assertTrue(date!!.isAfter(LocalDateTime.parse(ADVERTISEMENT_CREATION_DATE)))
     }
 
     @Test
     fun `should not update an advertisement with incorrect password`() {
 
-        withAnAdvertisementCreated()
+        withAnAdvertisementCreated {
+            val server = Server(DependencyInjectionResolver())
 
-        val server = Server(DependencyInjectionResolver())
-
-        val result = server.route(FrameworkRequest(
-                FrameworkRequest.METHOD_PUT,
-                "advertisement/$ID",
-                mapOf(
-                    "description" to NEW_DESCRIPTION,
-                    "password" to INCORRECT_PASSWORD,
+            val result = server.route(
+                FrameworkRequest(
+                    FrameworkRequest.METHOD_PUT,
+                    "advertisement/$ID",
+                    mapOf(
+                        "description" to NEW_DESCRIPTION,
+                        "password" to INCORRECT_PASSWORD,
+                    )
                 )
             )
-        )
 
-        Assertions.assertEquals(FrameworkResponse.STATUS_OK, result.statusCode)
+            Assertions.assertEquals(FrameworkResponse.STATUS_OK, result.statusCode)
 
-        val resultSet = this.connection.query("SELECT * from advertisements;")
-        var description = ""
-        var date: LocalDateTime? = null
+            val resultSet = this.connection.query("SELECT * from advertisements;")
+            var description = ""
+            var date: LocalDateTime? = null
 
-        if (resultSet.next()) {
-            description = resultSet.getString("description")
-            date = LocalDateTime.parse(resultSet.getString("advertisement_date"))
+            if (resultSet.next()) {
+                description = resultSet.getString("description")
+                date = LocalDateTime.parse(resultSet.getString("advertisement_date"))
+            }
+
+            Assertions.assertEquals(DESCRIPTION, description)
+            Assertions.assertTrue(date!!.isEqual(LocalDateTime.parse(ADVERTISEMENT_CREATION_DATE)))
         }
-
-        Assertions.assertEquals(DESCRIPTION, description)
-        Assertions.assertTrue(date!!.isEqual(LocalDateTime.parse(ADVERTISEMENT_CREATION_DATE)))
     }
 
     @Test
     fun `should not renew an advertisement with incorrect password`() {
+        withAnAdvertisementCreated {
+            val server = Server(DependencyInjectionResolver())
 
-        withAnAdvertisementCreated()
-
-        val server = Server(DependencyInjectionResolver())
-
-        val result = server.route(FrameworkRequest(
-                FrameworkRequest.METHOD_PATCH,
-                "advertisement/$ID",
-                mapOf(
-                    "password" to INCORRECT_PASSWORD,
+            val result = server.route(
+                FrameworkRequest(
+                    FrameworkRequest.METHOD_PATCH,
+                    "advertisement/$ID",
+                    mapOf(
+                        "password" to INCORRECT_PASSWORD,
+                    )
                 )
             )
-        )
 
-        Assertions.assertEquals(FrameworkResponse.STATUS_OK, result.statusCode)
+            Assertions.assertEquals(FrameworkResponse.STATUS_OK, result.statusCode)
 
-        val resultSet = this.connection.query("SELECT * from advertisements;")
-        var date: LocalDateTime? = null
+            val resultSet = this.connection.query("SELECT * from advertisements;")
+            var date: LocalDateTime? = null
 
-        if (resultSet.next()) {
-            date = LocalDateTime.parse(resultSet.getString("advertisement_date"))
+            if (resultSet.next()) {
+                date = LocalDateTime.parse(resultSet.getString("advertisement_date"))
+            }
+
+            Assertions.assertTrue(date!!.isEqual(LocalDateTime.parse(ADVERTISEMENT_CREATION_DATE)))
         }
-
-        Assertions.assertTrue(date!!.isEqual(LocalDateTime.parse(ADVERTISEMENT_CREATION_DATE)))
     }
 
-    private fun withAnAdvertisementCreated()
+    private fun withAnAdvertisementCreated(block: () -> Unit)
     {
-        val password = "myPassword".md5()
+        val password = PASSWORD.md5()
         val creationDate = LocalDateTime.parse(ADVERTISEMENT_CREATION_DATE).toString()
         this.connection.execute("INSERT INTO advertisements (id, description, password, advertisement_date)" +
                 " VALUES ('$ID', '$DESCRIPTION', '$password', '$creationDate')")
+
+        block()
     }
 
     private fun String.md5(): String {
