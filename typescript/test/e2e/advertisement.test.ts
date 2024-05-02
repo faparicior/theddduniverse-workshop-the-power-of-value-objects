424962cbb40fb48d7100485028ac0c44eee9caaa
Fernando Aparicio <faparicior@gmail.com>
2024-05-02 22:30:44 +0200

    Add tests in typescript

--- typescript/test/e2e/advertisement.test.ts
+++ typescript/test/e2e/advertisement.test.ts
@@ -1,4 +1,3 @@
-import { v4 as uuid } from "uuid"
 import { FrameworkRequest, Method } from "../../src/framework/FrameworkRequest";
 import { FrameworkServer } from "../../src/framework/FrameworkServer";
 import { SqliteConnectionFactory } from "../../src/framework/database/SqliteConnectionFactory";
@@ -7,12 +6,14 @@ import { createHash } from "node:crypto";
 
 let connection: DatabaseConnection;
 let server: FrameworkServer
-const FLAT_ID = '6fa00b21-2930-483e-b610-d6b0e5b19b29';
+const ID = '6fa00b21-2930-483e-b610-d6b0e5b19b29';
 const ADVERTISEMENT_CREATION_DATE = '2024-02-03 13:30:23';
+const DESCRIPTION = 'Dream advertisement';
+const PASSWORD = 'myPassword';
+const NEW_DESCRIPTION = 'Dream advertisement changed';
+const INCORRECT_PASSWORD = 'myBadPassword';
 
 describe("Advertisement", () => {
-
-
     beforeAll(async () => {
         connection = await SqliteConnectionFactory.createClient();
         server = await FrameworkServer.start();
@@ -23,13 +24,9 @@ describe("Advertisement", () => {
         await connection.execute('delete from advertisements;', [])
     })
 
-    it("Should publish a advertisement", async () => {
-
-        const description = 'Dream advertisement'
-        const id = uuid()
-
+    it("Should publish an advertisement", async () => {
         const request = new FrameworkRequest(Method.POST, '/advertisement',
-            { id, description, password: 'myPassword' }
+            { id: ID, description: DESCRIPTION, password: PASSWORD }
         )
 
         const response = await server.route(request)
@@ -39,21 +36,18 @@ describe("Advertisement", () => {
         const dbData = await connection.query("SELECT * FROM advertisements") as any[]
 
         expect(dbData.length).toBe(1);
-        expect(dbData[0].id).toBe(id);
-        expect(dbData[0].description).toBe(description);
+        expect(dbData[0].id).toBe(ID);
+        expect(dbData[0].description).toBe(DESCRIPTION);
         expect(dbData[0].password).toBeDefined;
         expect(dbData[0].advertisement_date).toBeDefined;
-
     });
 
 
     it("Should change an advertisement", async () => {
         await withAnAdvertisementCreated()
 
-        const newDescription = 'Dream advertisement changed'
-
-        const request = new FrameworkRequest(Method.PUT, `/advertisements/${FLAT_ID}`,
-            { description: newDescription, password: 'myPassword' }
+        const request = new FrameworkRequest(Method.PUT, `/advertisements/${ID}`,
+            { description: NEW_DESCRIPTION, password: PASSWORD }
         )
 
         const response = await server.route(request)
@@ -64,20 +58,17 @@ describe("Advertisement", () => {
         const dbData = await connection.query("SELECT * FROM advertisements") as any[]
 
         expect(dbData.length).toBe(1);
-        expect(dbData[0].description).toBe(newDescription);
+        expect(dbData[0].description).toBe(NEW_DESCRIPTION);
         const newDate = new Date(dbData[0].advertisement_date)
         const diff = getHourDifference(newDate)
         expect(diff).toBeLessThan(1)
-
     })
 
     it("Should renew an advertisement", async () => {
         await withAnAdvertisementCreated()
 
-        const newDescription = 'Dream advertisement'
-
-        const request = new FrameworkRequest(Method.PATCH, `/advertisements/${FLAT_ID}`,
-            { password: 'myPassword' }
+        const request = new FrameworkRequest(Method.PATCH, `/advertisements/${ID}`,
+            { password: PASSWORD }
         )
 
         const response = await server.route(request)
@@ -88,25 +79,65 @@ describe("Advertisement", () => {
         const dbData = await connection.query("SELECT * FROM advertisements") as any[]
 
         expect(dbData.length).toBe(1);
-        expect(dbData[0].description).toBe(newDescription);
         const newDate = new Date(dbData[0].advertisement_date)
         const diff = getHourDifference(newDate)
         expect(diff).toBeLessThan(1)
     })
+
+    it("Should not change an advertisement with incorrect password", async () => {
+        await withAnAdvertisementCreated()
+
+        const request = new FrameworkRequest(Method.PUT, `/advertisements/${ID}`,
+            { description: NEW_DESCRIPTION, password: INCORRECT_PASSWORD }
+        )
+
+        const response = await server.route(request)
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toBeUndefined;
+
+        const dbData = await connection.query("SELECT * FROM advertisements") as any[]
+
+        expect(dbData.length).toBe(1);
+        expect(dbData[0].description).toBe(DESCRIPTION);
+        const newDate = new Date(dbData[0].advertisement_date)
+        const diff = getHourDifference(newDate)
+        expect(diff).toBeGreaterThan(1)
+    })
+
+    it("Should not renew an advertisement with incorrect password", async () => {
+        await withAnAdvertisementCreated()
+
+        const request = new FrameworkRequest(Method.PATCH, `/advertisements/${ID}`,
+            { password: INCORRECT_PASSWORD }
+        )
+
+        const response = await server.route(request)
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toBeUndefined;
+
+        const dbData = await connection.query("SELECT * FROM advertisements") as any[]
+
+        expect(dbData.length).toBe(1);
+        expect(dbData[0].description).toBe(DESCRIPTION);
+        const newDate = new Date(dbData[0].advertisement_date)
+        const diff = getHourDifference(newDate)
+        expect(diff).toBeGreaterThan(1)
+    })
+
 });
 
 
 async function withAnAdvertisementCreated(): Promise<void> {
-
     await connection.execute(
         `INSERT INTO advertisements (id, description, password, advertisement_date) VALUES (?, ?, ?, ?)`,
         [
-            FLAT_ID,
-            'Dream advertisement',
-            createHash('md5').update('myPassword').digest('hex'),
+            ID,
+            DESCRIPTION,
+            createHash('md5').update(PASSWORD).digest('hex'),
             ADVERTISEMENT_CREATION_DATE
         ]);
-
 }
 
 function getHourDifference(date: Date): number {
